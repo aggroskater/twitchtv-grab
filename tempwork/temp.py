3dd3be6f733cf25b34d393429f0bfc7ec09e2a13
Preston Maness <aggroskater@gmail.com>
2014-08-26 17:17:11 -0500

    Beginning to clean up some of my messy print statements from when I was first aquainting myself with python.

--- tempwork/temp.py
+++ tempwork/temp.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python
 
 import warc
-from cStringIO import StringIO #for converting payloads into binary strings
-# that reside in-memory rather than on-file
-import os #for file length, setting environment variables
-from subprocess import call #for executing ffmpeg
-import shlex #for properly parsing command lines for insertion into call()s
-import glob #for grabbing all *.jpg files
+from cStringIO import StringIO # for converting payloads into binary strings
+                               # that reside in-memory rather than on-file
+import os # for file length, setting environment variables
+from subprocess import call # for executing ffmpeg
+import shlex # for properly parsing command lines for insertion into call()s
+import glob # for grabbing all *.jpg files
 
 # f will hold array of records?
 # No. f holds a WARCFile object, and has an iterator that invokes a
 # WARCReader object, which in turn invokes a WARCHeader object (and WARCRecord
-# object?), which is ultiamtely what we want to look at to identify records.
-#f = warc.open("twitchtv-393d967f1c17b787e5f0756907b6e514621f16c2-20140815-115402.warc.gz")
-#f = warc.open("test.warc.gz")
-#f = warc.open("cloned.warc.gz")
-#f = warc.open("wtf.warc.gz")
+# object?), which is ultimately what we want to look at to identify records.
 f = warc.open("twitchtv-8bbd60023627ec4a666c026a38b0b587bcf9fcb3-20140817-233758.warc.gz")
-
-#print "f is: ", f
-
-#newfile = open("truncated.warc.gz","w")
-#newfile = warc.gzip2.GzipFile(filename="truncated.warc.gz", mode="w")
 
 # If you open a new warcfile with warc.open, and of a filename ending in
 # "*.gz", then the warc library will handle writing the compressed data
@@ -52,12 +43,9 @@ newfile = warc.open("truncated.warc.gz","w")
 # * From the truncated response record: WARC-Record-ID
 #
 #   This will be set as the "WARC-Refers-To" header in the conversion records.
-
 warcinfo_record_ID = None
 metadata_record_ID = None 
 truncated_record_ID = None 
-
-#print "newfile is: ", newfile
 
 # each record is an object, and each object has a header, which is a dictionary.
 # some keys are mandatory and guaranteed to be there (warc-type, content-type,
@@ -65,36 +53,47 @@ truncated_record_ID = None
 # make sure it exists before asking for it.
 for record in f:
 
-    print "WARC-Type: ", record['warc-type']
-    print "WARC-Content-Type: ", record['content-type']
-    print "Content-Length: ", record['Content-Length']
+#    print "WARC-Type: ", record['warc-type']
+#    print "WARC-Content-Type: ", record['content-type']
+#    print "Content-Length: ", record['Content-Length']
     
-    for key in record.header.keys():
+#    for key in record.header.keys():
 #        print key, ": ",record[key]
-        if key == "warc-target-uri":
-            print "WARC-Target-URI:", record['warc-target-uri']
-        if key == "warc-identified-payload-type":
-            print "WARC-Identified-Payload-Type: ", record['warc-identified-payload-type']
+#        if key == "warc-target-uri":
+#            print "WARC-Target-URI:", record['warc-target-uri']
+#        if key == "warc-identified-payload-type":
+#            print "WARC-Identified-Payload-Type: ", record['warc-identified-payload-type']
 
 # grab the lengthy payload (that probably contains the video)
     if long(record['Content-Length']) >= 500000:
 
         # grab the truncated record id (will use in later conversion records)
         truncated_record_ID = record['warc-record-id']
+
         #add truncated header
-        print "Adding warc-truncated header."
+#        print "Adding warc-truncated header."
         record['warc-truncated'] = "length"
 
         #extract the payload
-        print "Time to grab the payload."
+#        print "Time to grab the payload."
         tempfile = open("intermediate.int", 'wb')
-        print "Writing to intermediate.int"
+#        print "Writing to intermediate.int"
         for line in record.payload:
             tempfile.write(line)
-        print "Done writing to intermediate.int"   
+#        print "Done writing to intermediate.int"   
         tempfile.close()
  
-        # put the payload back
+        # put the payload back; iterating through record.payload
+        # invokes a generator on the payload that seems to
+        # "eat up" the payload in the original file. I say so because
+        # attempting to, say, write the payload out twice (to TWO files)
+        # will fail, as will any attempt to read out the payload again
+        # without first "putting it back." (I'd love an explanation for
+        # just what's going on here; but for now, this hack works)
+        # (for the record with the long content-length, we end up reading
+        # the payload twice; once here, to get it to a separate file, and
+        # once again, in COPY PAYLOAD, to write out a truncated version to
+        # the new warc file)
         tempfile2 = open("intermediate.int", 'rb')
         data = tempfile2.read()
         stream = StringIO(data)
@@ -106,7 +105,8 @@ for record in f:
         #stream.close()
         # "IO Operation on a closed file"
 
-# adjust the warcinfo record to include additional information
+# adjust the warcinfo record to include additional information and grab its
+# warc-record-id
     elif (record['WARC-Type'] == "warcinfo"):
 
         # grab the record-id for later use in resource records
@@ -116,53 +116,47 @@ for record in f:
         # warcinfo record that indicates the use of ffmpeg.
         #asdf
 
+# grab the metadata record's warc-record-id for later resource records
     elif (record['WARC-Type'] == "metadata"):
         metadata_record_ID = record['warc-record-id']
 
+# BEGIN CREATING NEW RECORD
 
-# change headers
-#    temp = record['content-type']
-#    record['content-type'] = "lolwutermelon"
-#    print "WARC-Content-Type: ", record['content-type']        
-#    record['content-type'] = temp
-#    print "WARC-Content-Type: ", record['content-type']
-
-# won't work to get payload
-# resulted in "grab the payload" above
-#    print "Record's payload is: ", record.payload
-#    print "Record is: ", record
-#    for j in record:
-#        print "j is: ", j
-
-# copy-paste records to new file
-
+    # COPY HEADER
     new_header = warc.WARCHeader(record.header)
 
+    # COPY PAYLOAD
+
+    # if the current record gets truncated, let the subsequent record
+    # creation know about it to set an appropriate header
     truncated_flag = None
 
+    # short record payloads
     if long(record['content-length']) < 500000:
 
-        print "Copying payload..."
+#        print "Copying payload..."
         new_payload = StringIO()
         for line in record.payload:
             new_payload.write(line)
         #if we don't seek back to 0, new_payload.read() is empty
         new_payload.seek(0)
-        print "Done copying payload."
+#        print "Done copying payload."
 
+    # long record payloads (the one that probably has video data)
     else:
-        print "Found long content-length. Truncating..."
+#        print "Found long content-length. Truncating..."
         new_payload = StringIO()
         decrement = 25
         #Grab some lines
-        print "Gonna grab some lines. Decrement: ", decrement
+#        print "Gonna grab some lines. Decrement: ", decrement
         for line in record.payload:
             print "Grabbing a line."
             new_payload.write(line)
             decrement -= 1
-            print "Decrement: ", decrement
+#            print "Decrement: ", decrement
             if decrement == 0:
                 break
+        # be kind: rewind
         new_payload.seek(0)
         truncated_flag = True
 
@@ -174,6 +168,9 @@ for record in f:
     # (incidentally, wget-lua probably *should* be setting the
     # warc-payload-digest header, but that is neither here nor there at the
     # moment)
+
+    # CREATE RECORD FROM HEADER AND PAYLOAD
+
     new_rec = warc.WARCRecord(payload=new_payload.read(), headers=new_header, defaults=False)
 
     if truncated_flag:
@@ -193,12 +190,21 @@ for record in f:
         thelength = new_payload.tell()
         new_rec['content-length'] = str(thelength)
         new_payload.seek(0)
+
+    # WRITE THE NEW RECORD OUT TO THE NEW WARCFILE
+    # (the warc library handles the gz-compression and putting each record
+    # in a separate gz "member" transparently; no need to much with the gzip
+    # library ourselves)
  
-    print "Copying record to new .warc.gz"
+#    print "Copying record to new .warc.gz"
     newfile.write_record(new_rec)
-    print "Done copying record to new .warc.gz"
+#    print "Done copying record to new .warc.gz"
 
     print "\n\n"
+
+# END OF MAIN FOR LOOP; at this point, we have a new warcfile with
+# copied and truncated records; now, we need to sample the content and add
+# these "conversion" records to the warc file.
 
 # Should probably delete old warc at this point, since new warcfile has all
 # of the old records, and we've already got another copy of the main
@@ -208,7 +214,10 @@ for record in f:
 # to have more of a generator-like setup that negates the need to use
 # twice the disk space, but it's beyond the scope of my abilities at the
 # moment and I don't think I'd be able to get up to speed before the
-# deadline for this project drops (August 27 2014))
+# deadline for this project drops (August 27 2014) Update: LOL Twitch is
+# already deleting things on August 26; oh well, I suppose this code
+# could come in handy if the IA suddenly needs to compress lots of
+# material)
 
 # Now, we need to convert the flv, and add conversion records
 
@@ -216,7 +225,6 @@ for record in f:
 # HTTP Response headers. We need to grep for "CRLFCRLF" and then chop off
 # anything prior to it, including it, leaving nothing but the flv file for
 # ffmpeg to work with.
-
 thefile = open("intermediate.int").read() # NOT A FILE; just a "str"
 theflv = thefile.split('\r\n\r\n')[1]
 
