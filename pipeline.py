0d713182e2c4270b7de30badd41b4123b62bd122
Preston Maness <aggroskater@gmail.com>
2014-09-10 12:44:36 -0500

    Initial migration into the pipeline infrastructure complete! It probably doesn't compile. I'll figure out testing (now that the tracker is down -__-) later tonight to make sure everything WOULD run in the event of a live project.

--- pipeline.py
+++ pipeline.py
@@ -175,7 +175,7 @@ class PrepareDirectories(SimpleTask):
 # use to someone in the future.
 class Sample(SimpleTask):
     def __init__(self):
-        SimpleTask__init__(self, "Sample")
+        SimpleTask.__init__(self, "Sample")
 
     def process(self, item):
 
@@ -225,20 +225,85 @@ class Sample(SimpleTask):
         old_warc_file = warc.open("%(warc_file_base)s.warc.gz" % item)
         new_warc_file = warc.open("%(warc_file_base)s-POSTPROCESSED.warc.gz" % item , "w")
 
+        # ------------------------ Start of main for loop -------------------#
+
         # and here... we... go
         for record in old_warc_file:
+
+            # Firstly, we detect whether the record we're iterating over holds
+            # data we'll need later. If so, behave appropriately. After the
+            # if-elif-elif dance, we proceed to copy each record into a new
+            # record in the %(warc_file_base)s-POSTPROCESSED.warc.gz file,
+            # modifying as necesary (truncated long records, etc)
+
+            # ------------------------ Check for data -------------------------#
 
             # Grab the lengthy payload (the flv file); if the content-length is
             # longer than ~5MiB, and the record is of the "response" type, then
             # this record *probably* has the flv file.
             if ((long(record['Content-Length']) >= 5000000) && record['WARC-Type'] == "response"):
 
-                #asdf
+                # need the record id of the original flv record. Will refernece
+                # it in truncated record.
+                truncated_record_id = record['warc-record-id']
+
+                # add "WARC-Truncated" to this record, indicating that it has
+                # been truncated due to length.
+                record['warc-truncated'] = "length"
+
+                # extract the payload
+                tempfile = open("intermediate.int", 'wb')
+                for line in record.payload:
+                    tempfile.write(line)
+                tempfile.close()
+
+                # put the payload back; iterating through record.payload
+                # invokes a generator on the payload that seems to
+                # "eat up" the payload in the original file. I say so because
+                # attempting to, say, write the payload out twice (to TWO files)
+                # will fail, as will any attempt to read out the payload again
+                # without first "putting it back." (I'd love an explanation for
+                # just what's going on here; but for now, this hack works)
+                # (for the record with the long content-length, we end up reading
+                # the payload twice; once here, to get it to a separate file, and
+                # once again, in COPY PAYLOAD, to write out a truncated version to
+                # the new warc file)
+                stream = StringIO(open("intermediate.dat", 'rb').read())
+                stream.seek(0, os.SEEK_END)
+                streamlength = stream.tell()
+                stream.seek(0)
+                record.payload = warc.utils.FilePart(fileobj = stream, length = streamlength)
+
+                # can't close the stream yet for some reason. This might
+                # introduce leaks of some sort, so keep an eye on it.
+                # The relevant error: "IO Operation on a closed file."
+                # I suspect this operation occurs somewhere in the warc library,
+                # and i'm hoping that the stream object just falls out of scope
+                # at some point other than when the entire pipeline shuts down.
+                # stream.close()
 
             # Adjust the warcinfo record to note that we also utilized ffmpeg
             elif (record['WARC-Type'] == "warcinfo"):
 
-                #asdf
+                # grab the record-id for later use in resource records
+                warcinfo_record_ID = record['warc-record-id']
+
+                # gotta add another "software" key to the content-block of the
+                # warcinfo record that indicates the use of ffmpeg.
+                warcinfo_stream = StringIO()
+                for line in record.payload:
+                    warcinfo_stream.write(line)
+
+                # trailing \r\n\r\n is already present in the payload; just seek back
+                # two bytes (yes, the second \r\n will get clobbered; potential unicode
+                # byte-length issues here) and then tack on the additional lines you
+                # need to like so:
+                warcinfo_stream.seek(-2,os.SEEK_END)
+                warcinfo_stream.write("software: ffmpeg/2.3.1\r\n\r\n")
+                warcinfo_stream.seek(0, os.SEEK_END)
+                warcinfo_stream_len = warcinfo_stream.tell()
+                warcinfo_stream.seek(0)
+                record.payload = warc.utils.FilePart(fileobj=warcinfo_stream,length=warcinfo_stream_len)
 
             # Get the metadata record's warc-record-id for later resource
             # records.
@@ -246,49 +311,137 @@ class Sample(SimpleTask):
 
                 metadata_record_ID = record['warc-record-id']
 
-        # end of "for record in old_warc_file"
+            # End of conditionals. Proceed to write the new record to the
+            # post-processed warcfile.
 
-        # Extract the actual flv file; as of now, the "intermediate.int"
-        # still holds the response headers
+            # ------------------------ Copy Record -------------------------#
 
-        # Oh dear. It's looking like manipulating the warc file would
-        # require more careful deliberation than just "sample the data
-        # and put it back into the warc." Lots of metadata.
-        #
-        # http://warc.readthedocs.org/en/latest/
-        #
-        # It looks like "WARC-Truncated" is the named field we would want
-        # according to the WARC spec:
-        #
-        # http://bibnum.bnf.fr/WARC/WARC_ISO_28500_version1_latestdraft.pdf
-        #
-        # Could rename whichever record in the .warc.gz holds the flv
-        # (or other video file)
-        # 
-        # Planned method of attack:
-        #
-        # 0.) Unpack the .warc.gz
-        # 1.) Extract the video payload of the original record.
-        # 2.) Sample the video payload with native-resolution snapshots.
-        # (SnapShot() helper) 
-        # 3.) Shrink the video payload. (ShrinkRay() helper)
-        # 4.) Create new record with "conversion" profile, 
-        # referencing the original record with WARC-Refers-To header. Store
-        # this snapshot material in the new record (in a tarball or
-        # something? Would a metadata record be useful to describe how to
-        # handle the tarball? (Lots of different ways to display slideshows,
-        # which is basically what this is.))
-        # 5.) Replace original record payload with shrunken payload, mark
-        # the original record as WARC-Truncated.
-        # 6.) Repack the .warc.gz
+            # COPY HEADER
 
-        # NOTE TO SELF: the payloads of record objects INCLUDE the HTTP
-        # response headers before the actual content response.
-        # NOTE TO SELF: Can extract uncompressed payloads directly from
-        # compressed warcs (.warc.gz files) ; hoping that writing records
-        # is similarly capable.
+            # Should we add defaults=False ? It seems that some additional headers
+            # are added in WARCHeader as well as WARCRecord. However, they don't
+            # seem harmful: digests and timestamps.
+            new_header = warc.WARCHeader(record.header)
 
-        # should probably also report compression statistics to the tracker
+            # COPY PAYLOAD
+
+            # if the current record gets truncated, then set the content-length
+            # to the new, truncated length as per spec.
+            truncated_flag = None
+
+            # SHORT record payloads
+            if long(record['content-length']) < 500000:
+
+                #print "Copying payload..."
+                new_payload = StringIO()
+                for line in record.payload:
+                    new_payload.write(line)
+                #if we don't seek back to 0, new_payload.read() is empty
+                new_payload.seek(0)
+                #print "Done copying payload."
+
+            # LONG record payloads (the one that probably has video data)
+            else:
+
+            #print "Found long content-length. Truncating..."
+            new_payload = StringIO()
+            decrement = 25
+            #Grab some lines
+            #print "Gonna grab some lines. Decrement: ", decrement
+            for line in record.payload:
+                #print "Grabbing a line."
+                new_payload.write(line)
+                decrement -= 1
+                #print "Decrement: ", decrement
+                if decrement == 0:
+                    break
+            # be kind: rewind
+            new_payload.seek(0)
+            truncated_flag = True
+
+            #print "Done truncating."
+
+            # CREATE RECORD FROM HEADER AND PAYLOAD
+
+            new_rec = warc.WARCRecord(payload=new_payload.read(), headers=new_header, defaults=False)
+
+            # if this record happened to be one that got truncated, then we
+            # need to adjust its content-length header.
+            if truncated_flag:
+
+                #print "Adjusting content-length header"
+
+                # From page 9 of the ISO WARC Standard:
+                #
+                # "The WARC-Truncated field may be used on any WARC record. The WARC
+                # field Content-Length shall still report the actual truncated size of
+                # the record block."
+
+                # Get the length of the truncated content-block and set
+                # Content-Length header appropriately
+                new_payload.seek(0)
+                new_payload.seek(0, os.SEEK_END)
+                thelength = new_payload.tell()
+                new_rec['content-length'] = str(thelength)
+                new_payload.seek(0)
+
+            # WRITE THE NEW RECORD OUT TO THE NEW WARCFILE
+
+            # (the warc library handles the gz-compression and putting each record
+            # in a separate gz "member" transparently; no need to much with the gzip
+            # library ourselves)
+
+            #print "Copying record to new .warc.gz"
+            new_warc_file.write_record(new_rec)
+            #print "Done copying record to new .warc.gz"
+            #print "\n\n"
+
+        #------------------------ END OF MAIN FOR LOOP ------------------------#
+
+        # at this point, we have a new warcfile with copied and truncated
+        # records; now, we need to sample the content and add these "conversion"
+        # records to the warc file.
+
+        # Should probably delete old warc at this point, since new warcfile has all
+        # of the old records, and we've already got another copy of the main
+        # payload. If we proceed to write out the full newfile with the shrunken
+        # payload before deleting the old warc, we'll basically be using nearly
+        # 3x the interim diskspace rather than 2x. (Don't get me wrong, I'd love
+        # to have more of a generator-like setup that negates the need to use
+        # twice the disk space, but it's beyond the scope of my abilities at the
+        # moment and I don't think I'd be able to get up to speed before the
+        # deadline for this project drops (August 27 2014) Update: LOL Twitch is
+        # already deleting things on August 26; oh well, I suppose this code
+        # could come in handy if the IA suddenly needs to compress lots of
+        # material)
+
+        # Now, we need to convert the flv, and add conversion records
+
+        # Our "payload.flv" is not quite an flv yet; the payload still includes the
+        # HTTP Response headers. We need to grep for "CRLFCRLF" and then chop off
+        # anything prior to it, including it, leaving nothing but the flv file for
+        # ffmpeg to work with.
+        thefile = open("intermediate.int").read() # NOT A FILE; just a "str"
+        theflv = thefile.split('\r\n\r\n')[1]
+        writetheflv = open("samplethis.flv", "w")
+        writetheflv.write(theflv)
+        writetheflv.close()
+
+        # Get Snapshots
+        SnapShot()
+
+        # Get shrinked video
+        ShrinkRay()
+
+        # Clean up
+        print "********************* \n\n Removing temporary files; cleaning up \n\n*********************"
+        # remove original file intermediates: "intermediate.int" and "samplethis.flv"
+        rmargs = shlex.split("rm intermediate.int samplethis.flv")
+        call(rmargs)
+
+        # And we're done!
+        new_warc_file.close()
+        os.chdir(original_path)
 
     ###################
     # Sampling routines
@@ -297,27 +450,118 @@ class Sample(SimpleTask):
     # High fidelity snapshots
     def SnapShot():
 
+        # TODO:
         # figure out length of video and develop native-resolution frame
         # sampling rate based off of this length.
 
-        # begin to sample the video with ffmpeg using options determined
-        # from the length of video
-        call(["ffmpeg", ""])
+        print "********************* \n\n Getting snapshots. \n\n*********************"
 
-        # assert that ffmpeg exited successfully
+        os.environ["FFREPORT"] = "file=ffmpeg-snapshots.log"
 
+        # snapshot
+        # This is the "proper" way to handle complex command lines with lots of args
+        # https://stackoverflow.com/questions/8581140/python-subprocess-call-with-arguments-having-multiple-quotations
+        ffmpegsnapshotargs = shlex.split("ffmpeg -i samplethis.flv -vf fps=fps=1/15 -f image2 -q:v 1 images%05d.jpg")
+        call(ffmpegsnapshotargs)
+
+        print "********************* \n\n Compressing snapshots. \n\n*********************"
+
+        imagelist = glob.glob("*.jpg")
+        imageliststring = ' '.join(imagelist)
+        tarcommand = "tar -czvf snapshots.tar.gz " + imageliststring
+
+        # compress all the snapshots
+        tarargs = shlex.split(tarcommand)
+        call(tarargs)
+
+        # delete jpgs
+        rmcommand = "rm " + imageliststring
+        rmargs = shlex.split(rmcommand)
+        call(rmargs)
+
+        os.environ["FFREPORT"] = ""
+
+        # Add ffmpeg log record
+        ffmpegsampleheader = warc.WARCHeader({
+            "WARC-Type": "resource",
+            "WARC-Warcinfo-ID": warcinfo_record_ID,
+            "Content-Type": "text/plain",
+            "WARC-Concurrent-To": metadata_record_ID
+        })
+        ffmpegsamplepayload = StringIO(open("ffmpeg-snapshots.log").read()).getvalue()
+        ffmpegsamplerecord = warc.WARCRecord(headers=ffmpegsampleheader,payload=ffmpegsamplepayload)
+        new_warc_file.write_record(ffmpegsamplerecord)
+
+        # Add the actual snapshot record
+        snapshotrecord = warc.WARCRecord(
+            headers = warc.WARCHeader({
+                "WARC-Type": "conversion",
+                "Content-Type": "application/x-gtar",
+                "WARC-Refers-To": truncated_record_ID
+            }),
+            payload = StringIO(open("snapshots.tar.gz").read()).getvalue()
+        )
+        new_warc_file.write_record(snapshotrecord)
+
+        # remove snapshots and log
+        call(shlex.split("rm snapshots.tar.gz ffmpeg-snapshots.log"))
+
+        # end of SnapShot()
 
     # Low fidelity, shrinked video
     def ShrinkRay():
 
+        # TODO:
         # figure out length of video and develop number of frames to
         # drop out of every FPS interval.
 
-        # begin to shrink the video with ffmpeg using options determined
-        # from length of video
-        call(["ffmpeg", ""])
+        print "********************* \n\n Shrinking Video. (This will take a while) \n\n*********************"
 
-        # assert that ffmpeg exited successfully
+        os.environ["FFREPORT"] = "file=ffmpeg-shrinking.log"
+
+        # shrink; using the webm format at this resolution cuts the file size by
+        # *about* an order of magnitude, while still maintaining more-or-less
+        # perfectly crisp detail and motion. I'm thinking we don't need to drop
+        # frames, and that cutting the resolution down to this ~240P-level
+        # resolution is good enough.
+
+        # We really need to check for resolution and select an output resolution
+        # appropriately; this one-liner only works for 16:9 inputs
+
+        ffmpegshrinkargs = shlex.split("ffmpeg -i samplethis.flv -c:v libvpx -b:v 500K -c:a libvorbis -s 432x243 shrunken-to-webm.webm")
+        call(ffmpegshrinkargs)
+
+        # The final size of snapshots and shrunken video is anywhere from a fifth to
+        # a seventh of the original file size.
+
+        os.environ["FFREPORT"] = ""
+
+        # add ffmpeg log record
+        ffmpegshrinkheader = warc.WARCHeader({
+            "WARC-Type": "resource",
+            "WARC-Warcinfo-ID": warcinfo_record_ID,
+            "Content-Type": "text/plain",
+            "WARC-Concurrent-To": metadata_record_ID
+        })
+        ffmpegshrinkpayload = StringIO(open("ffmpeg-shrinking.log").read()).getvalue()
+        ffmpegshrinkrecord = warc.WARCRecord(headers=ffmpegshrinkheader,payload=ffmpegshrinkpayload)
+        new_warc_file.write_record(ffmpegshrinkrecord)
+
+        # add actual shrunken webm record
+        shrinkrecord = warc.WARCRecord(
+            headers = warc.WARCHeader({
+                "WARC-Type": "conversion",
+                "Content-Type": "video/webm",
+                "WARC-Refers-To": truncated_record_ID
+            }),
+            payload = StringIO(open("shrunken-to-webm.webm").read()).getvalue()
+        )
+        new_warc_file.write_record(shrinkrecord)
+
+        # remove log file
+        call(shlex.split("rm snapshots.tar.gz ffmpeg-shrinking.log"))
+
+        # end of ShrinkRay()
 
     #end of class Sample(SimpleTask)
 
